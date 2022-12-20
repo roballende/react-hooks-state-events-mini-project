14639dd68040c9770787747c0925d3f23891ebee
Robert Allende <allende.rob@gmail.com>
2022-12-20 03:21:30 -0600

    commit tree 372cd124547f0f9f7d5a2429df38c1ad7db5d112

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,24 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+
+  const handleDeleteTask = (deletedTask) => {
+  const filteredTasks = tasks.filter(task => deletedTask.text !== task.text)
+  setTasks(filteredTasks)
+  }
+
+  const handleAddNewTask = (newTask) => {
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <NewTaskForm categories={categories} handleAddNewTask={handleAddNewTask}/>
+      <TaskList tasks={tasks} handleDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({task, handleDeleteTask}) {
+
+  // const handleDelete = () => {
+  //   handleDeleteTask(task)
+  // }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button onClick={(e) => handleDeleteTask(task)} className="delete">X</button>
     </div>
   );
 }
